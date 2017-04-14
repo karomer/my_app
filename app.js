0303f373e26c127dd1b6e3bd68e0373d5f35b71f
karomer <ndiesh22@gmail.com>
2017-04-14 15:51:18 +0900

    MONGODB

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 var express = require('express');
 //express module을 express라는 이름의 변수로 저장
-
 var path = require('path');
 var app = express();
 // express()는 express module은 함수, 생성자
 // express를 사용하여 app을 생성
+var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
+
+mongoose.connect("mongodb://admin:1234@cluster0-shard-00-00-tvgvc.mongodb.net:27017,cluster0-shard-00-01-tvgvc.mongodb.net:27017,cluster0-shard-00-02-tvgvc.mongodb.net:27017/Cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
+var db = mongoose.connection;
+db.once("open", function (){
+console.log("DB connected!");
+});
+
+db.on("error", function(err){
+  console.log("DB ERROR :", err);
+});
+
+var dataSchema = mongoose.Schema({
+  name:String,
+  count:Number
+}); //mongoose.Schema()함수는 object를 인자로 받아 object를 스키마로 만든다. (이름:타입,이름:타입)
 
 app.set("view engine", 'ejs');
 // express에게 ejs를 view engine으로 사용할 것을 알림
-
-app.use(express.static(path.join(__dirname, '/pzublic')));
+app.use(express.static(path.join(__dirname, '/views')));
 //__dirname은 node에서 제공하는 node 파일의 경로를 담고 있는 변수
 
-var data ={count:0};
+var Data = mongoose.model('data',dataSchema);
+
 app.get('/',function(req,res){
+Data.findOne({name:"myData"}, function(err,data){ // 모델을 담는 변수는 대문자
+  if(err) return console.log("Data ERROR:",err);
   data.count++;
-  res.render('my_first_ejs',data);
+  data.save(function (err){
+    if(err) return console.log("Data ERROR:",err);
+    res.render('my_first_ejs',data);
+    });
+  });
 });
 
 app.get('/reset',function(req,res){
@@ -34,12 +56,26 @@ app.get('/set/:num',function(req,res){
 });
 // '/' route를 생성하고 '/'에 route에 get 신호가 오면 my_first_ejs파일을 render
 
-/*
-app.get('/',function(req,res){
-  res.send('Hello World!');
-});
-// 서버의 root에 get요청이 왔을때 Hello World라는 신호를 클라이언트에 보내라는 명령
-*/
+function setCounter(res,num) {
+  console.log("setCounter");
+  Data.findOne({name:"myData"}, function(err,data){ // 모델을 담는 변수는 대문자
+    if(err) return console.log("Data ERROR:",err);
+    data.count=num;
+    data.save(function (err){
+      if(err) return console.log("Data ERROR:",err);
+      res.render('my_first_ejs',data);
+      });
+    });
+}
+
+function getCounter(res) {
+  console.log("getCounter");
+  Data.findOne({name:"myData"}, function(err,data){ // 모델을 담는 변수는 대문자
+    if(err) return console.log("Data ERROR:",err);
+    res.render('my_first_ejs',data);
+    });
+}
+
 
 app.listen(8080, function() {
   console.log('Server On!');
